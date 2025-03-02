c835db95ddda87a7cbe024000d71e0bbfefe6c5c
Abhishek Behera <abhisekh.behera2018@gmail.com>
2025-03-02 14:36:30 +0530

    removed username &. password

--- vars/docker-push.groovy
+++ vars/docker-push.groovy
@@ -1,5 +1,5 @@
 def call(String Project, String ImageTag) {
-  withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'DOCKERHUB_PASS', usernameVariable: 'DOCKERHUB_USER')]) {
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-cred')]) {
       sh """
         echo "\$DOCKERHUB_PASS" | docker login --username "\$DOCKERHUB_USER" --password-stdin
         docker build -t "\$DOCKERHUB_USER/${Project}:${ImageTag}" .
